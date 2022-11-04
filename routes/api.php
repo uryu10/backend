31858c7c7d4f070d183ca3d4e77d96501be1c111
Anio32q$3 <adrian.burelos@gmail.com>
2022-11-04 13:17:37 -0600

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\BookController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
